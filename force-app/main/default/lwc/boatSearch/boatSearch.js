c8f22c55d9c56d9db26fa9db148c4778c95c790a
roigan <ana.roig@adidas.com>
2021-09-09 12:45:24 +0200

    Parent component: button to create new boat + loading spinner (pending logic)

--- force-app/main/default/lwc/boatSearch/boatSearch.js
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -1,5 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class BoatSearch extends LightningElement {
 
+export default class BoatSearch extends NavigationMixin(LightningElement) {
+    isLoading = false;
+
+    // Handles loading event
+    handleLoading() { }
+
+    // Handles done loading event
+    handleDoneLoading() { }
+
+    // Handles search boat event
+    // This custom event comes from the form (child)
+    searchBoats(event) { }
+
+    createNewBoat() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Boat__c',
+                actionName: 'new'
+            }
+        });
+    }
 }
