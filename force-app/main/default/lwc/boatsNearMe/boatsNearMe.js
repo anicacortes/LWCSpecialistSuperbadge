d5a94c5d52426795a5943b5d10e6024253aac354
roigan <ana.roig@adidas.com>
2021-09-10 12:44:34 +0200

    New components Map and NearMe

---
+++ force-app/main/default/lwc/boatsNearMe/boatsNearMe.js
@@ -0,0 +1,7 @@
+import { LightningElement, api } from 'lwc';
+import getBoatsByLocation from '@salesforce/apex/BoatDataService.getBoatsByLocation';
+
+export default class BoatsNearMe extends LightningElement {
+
+    @api boatTypeId;
+}
