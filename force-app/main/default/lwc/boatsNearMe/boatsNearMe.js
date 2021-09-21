8bcbc7e5395cf16d01a48ca7155654ea5a1feae3
roigan <ana.roig@adidas.com>
2021-09-21 17:30:26 +0200

    Beginning of location logic

--- force-app/main/default/lwc/boatsNearMe/boatsNearMe.js
+++ force-app/main/default/lwc/boatsNearMe/boatsNearMe.js
@@ -1,7 +1,42 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
 import getBoatsByLocation from '@salesforce/apex/BoatDataService.getBoatsByLocation';
 
 export default class BoatsNearMe extends LightningElement {
 
     @api boatTypeId;
+    @track isRendered = true;
+    listMarkers = [];
+
+    renderedCallback() {
+        if(this.isRendered) {
+            this.isRendered = false;
+            this.getLocationFromBrowser();
+        }
+    }
+
+    getLocationFromBrowser() {
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(position => {
+                 // Get the Latitude and Longitude from Geolocation API
+                var latitude = position.coords.latitude;
+                var longitude = position.coords.longitude;
+
+                this.listMarkers = [{
+                    location: {
+                        Latitude: latitude,
+                        Longitude: longitude
+                    },
+                    title: 'You are here'
+                }];
+            })
+        }
+    }
+
+    @wire(getBoatsByLocation, {latitude: this.listMarkers[0].location.latitude, longitude: this.listMarkers[0].location.longitude, boatTypeId: this.boatTypeId})
+    wiredBoatsJSON({error, data}) {
+        if(data) {
+            console.log('>> result from apex location:');
+            console.log(data);
+        }
+    }
 }
